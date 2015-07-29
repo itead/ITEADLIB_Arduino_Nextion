8c0d6ad2a029f3fdf77173939631c6b60625e559
shennongmin <wupangfee@gmail.com>
2015-07-29 12:55:49 +0800

    Rename class name NexSlice to NexCrop.
    Signed-off-by: shennongmin <wupangfee@gmail.com>

---
+++ NexCrop.cpp
@@ -0,0 +1,85 @@
+/**
+ * @file NexCrop.cpp
+ *
+ * API of NexCrop. 
+ *
+ * @author  Wu Pengfei (email:<pengfei.wu@itead.cc>)
+ * @date    2015/7/10
+ * @copyright 
+ * Copyright (C) 2014-2015 ITEAD Intelligent Systems Co., Ltd. \n
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ */
+
+#include "NexCrop.h"
+
+/**
+ * Constructor,inherited NexTouch's constructor function.
+ *
+ */
+NexCrop::NexCrop(NexPid pid, NexCid cid, char *name, NexTouchEventCb pop, void *pop_ptr)
+    :NexTouch(pid, cid, name, pop, pop_ptr)
+{
+}
+
+/*
+ * Get the number of picture. 
+ *
+ * @param number - an output parameter to save the number of picture. 
+ * 
+ * @retval true - success. 
+ * @retval false - failed. 
+ */
+bool NexCrop::getPic(uint32_t *number)
+{
+    String cmd = String("get ");
+    cmd += getObjName();
+    cmd += ".picc";
+    sendCommand(cmd.c_str());
+    return recvRetNumber(number);
+}
+
+/*
+ * Set the number of picture. 
+ *
+ * @param number - the number of picture. 
+ * 
+ * @retval true - success. 
+ * @retval false - failed. 
+ */
+bool NexCrop::setPic(uint32_t number)
+{
+    char buf[10] = {0};
+    String cmd;
+    
+    utoa(number, buf, 10);
+    cmd += getObjName();
+    cmd += ".picc=";
+    cmd += buf;
+
+    sendCommand(cmd.c_str());
+    return recvRetCommandFinished();
+}
+
+/**
+ * Register crop pop callback function. 
+ *
+ * @param pop - the pointer to crop pop callback function.
+ * @param ptr - the parameter to be transmitted to crop pop callback function. 
+ */
+void NexCrop::attachPop(NexTouchEventCb pop, void *ptr)
+{
+    NexTouch::attachPop(pop, ptr);
+}
+
+/**
+ * Unload crop pop callback function.
+ *
+ */
+void NexCrop::detachPop(void)
+{
+    NexTouch::detachPop();   
+}
+ 
---
+++ NexCrop.h
@@ -0,0 +1,38 @@
+/**
+ * @file NexCrop.h
+ *
+ * API of NexCrop. 
+ *
+ * @author  Wu Pengfei (email:<pengfei.wu@itead.cc>)
+ * @date    2015/7/10
+ * @copyright 
+ * Copyright (C) 2014-2015 ITEAD Intelligent Systems Co., Ltd. \n
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ */
+
+#ifndef __NEXCROP_H__
+#define __NEXCROP_H__
+#ifdef __cplusplus
+#include "NexTouch.h"
+
+/**
+ * NexCrop,subclass of NexTouch,provides simple methods to control crop component. 
+ *
+ */
+class NexCrop: public NexTouch
+{
+public: /* methods */
+    NexCrop(NexPid pid, NexCid cid, char *name, NexTouchEventCb pop = NULL, void *pop_ptr = NULL);
+    
+    void attachPop(NexTouchEventCb pop, void *ptr = NULL);
+    void detachPop(void);
+
+    bool getPic(uint32_t *number);
+    bool setPic(uint32_t number);
+};
+
+#endif /* #ifdef __cplusplus */
+#endif /* #ifndef __NEXCROP_H__ */
