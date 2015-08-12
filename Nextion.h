0d2088fe97781601e7a6e8b0d2f1bd74d231c30f
shennongmin <wupangfee@gmail.com>
2015-08-12 16:19:17 +0800

    Add Nextion.h only one header.
    Signed-off-by: shennongmin <wupangfee@gmail.com>

---
+++ Nextion.h
@@ -0,0 +1,37 @@
+/**
+ * @file Nextion.h
+ *
+ * Header file of Nextion Library. 
+ *
+ * All examples or sketches need to include this file. 
+ *
+ * @author  Wu Pengfei (email:<pengfei.wu@itead.cc>)
+ * @date    2015/8/12
+ * @copyright 
+ * Copyright (C) 2014-2015 ITEAD Intelligent Systems Co., Ltd. \n
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ */
+
+#ifndef __NEXTION_H__
+#define __NEXTION_H__
+
+#include "Arduino.h"
+#include "NexSerialConfig"
+#include "NexTouch"
+#include "NexHardware"
+
+#include "NexButton"
+#include "NexCrop"
+#include "NexGauge"
+#include "NexHotspot"
+#include "NexPage"
+#include "NexPicture"
+#include "NexProgressBar"
+#include "NexSlider"
+#include "NexText"
+#include "NexWaveform"
+
+#endif /* #ifndef __NEXTION_H__ */
