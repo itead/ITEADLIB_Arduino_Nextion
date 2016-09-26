d6c42dbf6203dff21d2c1d70f84c9825b92806ea
huangxiaoming <xiaoming.huang@itead.cc>
2016-09-26 17:58:27 +0800

    add api for button

--- NexButton.cpp
+++ NexButton.cpp
@@ -41,3 +41,311 @@ bool NexButton::setText(const char *buffer)
     return recvRetCommandFinished();    
 }
 
+
+uint32_t NexButton::getBco(uint32_t *number)
+{
+	String cmd;
+	cmd += "get ";
+	cmd += getObjName();
+	cmd += ".bco";
+	sendCommand(cmd.c_str());
+	return recvRetNumber(number);
+}
+
+bool NexButton::setBco(uint32_t number)
+{
+	char buf[10] = {0};
+    String cmd;
+    
+    utoa(number, buf, 10);
+    cmd += getObjName();
+    cmd += ".bco=";
+    cmd += buf;
+    sendCommand(cmd.c_str());
+	
+	cmd="";
+	cmd += "ref ";
+	cmd += getObjName();
+	sendCommand(cmd.c_str());
+    return recvRetCommandFinished();
+}
+
+uint32_t NexButton::getBco2(uint32_t *number)
+{
+	String cmd;
+	cmd += "get ";
+	cmd += getObjName();
+	cmd += ".bco2";
+	sendCommand(cmd.c_str());
+	return recvRetNumber(number);
+}
+
+bool NexButton::setBco2(uint32_t number)
+{
+	char buf[10] = {0};
+    String cmd;
+    
+    utoa(number, buf, 10);
+    cmd += getObjName();
+    cmd += ".bco2=";
+    cmd += buf;
+    sendCommand(cmd.c_str());
+	
+	cmd="";
+	cmd += "ref ";
+	cmd += getObjName();
+	sendCommand(cmd.c_str());
+    return recvRetCommandFinished();
+}
+
+uint32_t NexButton::getPco(uint32_t *number)
+{
+	String cmd;
+	cmd += "get ";
+	cmd += getObjName();
+	cmd += ".pco";
+	sendCommand(cmd.c_str());
+	return recvRetNumber(number);
+}
+
+bool NexButton::setPco(uint32_t number)
+{
+	char buf[10] = {0};
+    String cmd;
+    
+    utoa(number, buf, 10);
+    cmd += getObjName();
+    cmd += ".pco=";
+    cmd += buf;
+    sendCommand(cmd.c_str());
+	
+	cmd = "";
+	cmd += "ref ";
+	cmd += getObjName();
+	sendCommand(cmd.c_str());
+    return recvRetCommandFinished();
+}
+
+uint32_t NexButton::getPco2(uint32_t *number)
+{
+	String cmd;
+	cmd += "get ";
+	cmd += getObjName();
+	cmd += ".pco2";
+	sendCommand(cmd.c_str());
+	return recvRetNumber(number);
+}
+
+bool NexButton::setPco2(uint32_t number)
+{
+	char buf[10] = {0};
+    String cmd;
+    
+    utoa(number, buf, 10);
+    cmd += getObjName();
+    cmd += ".pco2=";
+    cmd += buf;
+    sendCommand(cmd.c_str());
+	
+	cmd = "";
+	cmd += "ref ";
+	cmd += getObjName();
+	sendCommand(cmd.c_str());
+    return recvRetCommandFinished();
+}
+
+uint32_t NexButton::getXcen(uint32_t *number)
+{
+	String cmd;
+	cmd += "get ";
+	cmd += getObjName();
+	cmd += ".xcen";
+	sendCommand(cmd.c_str());
+	return recvRetNumber(number);
+}
+
+bool NexButton::setXcen(uint32_t number)
+{
+	char buf[10] = {0};
+    String cmd;
+    
+    utoa(number, buf, 10);
+    cmd += getObjName();
+    cmd += ".xcen=";
+    cmd += buf;
+    sendCommand(cmd.c_str());
+	
+	cmd = "";
+	cmd += "ref ";
+	cmd += getObjName();
+	sendCommand(cmd.c_str());
+    return recvRetCommandFinished();
+}
+
+uint32_t NexButton::getYcen(uint32_t *number)
+{
+	String cmd;
+	cmd += "get ";
+	cmd += getObjName();
+	cmd += ".ycen";
+	sendCommand(cmd.c_str());
+	return recvRetNumber(number);
+}
+
+bool NexButton::setYcen(uint32_t number)
+{
+	char buf[10] = {0};
+    String cmd;
+    
+    utoa(number, buf, 10);
+    cmd += getObjName();
+    cmd += ".ycen=";
+    cmd += buf;
+    sendCommand(cmd.c_str());
+	
+	cmd = "";
+	cmd += "ref ";
+	cmd += getObjName();
+	sendCommand(cmd.c_str());
+    return recvRetCommandFinished();
+}
+
+uint32_t NexButton::getFont(uint32_t *number)
+{
+	String cmd;
+	cmd += "get ";
+	cmd += getObjName();
+	cmd += ".font";
+	sendCommand(cmd.c_str());
+	return recvRetNumber(number);
+}
+
+bool NexButton::setFont(uint32_t number)
+{
+	char buf[10] = {0};
+    String cmd;
+    
+    utoa(number, buf, 10);
+    cmd += getObjName();
+    cmd += ".font=";
+    cmd += buf;
+    sendCommand(cmd.c_str());
+
+	cmd = "";
+	cmd += "ref ";
+	cmd += getObjName();
+	sendCommand(cmd.c_str());
+    return recvRetCommandFinished();
+}
+
+uint32_t NexButton::getPicc(uint32_t *number)
+{
+	String cmd;
+	cmd += "get ";
+	cmd += getObjName();
+	cmd += ".picc";
+	sendCommand(cmd.c_str());
+	return recvRetNumber(number);
+}
+
+bool NexButton::setPicc(uint32_t number)
+{
+	char buf[10] = {0};
+    String cmd;
+    
+    utoa(number, buf, 10);
+    cmd += getObjName();
+    cmd += ".picc=";
+    cmd += buf;
+    sendCommand(cmd.c_str());
+	
+	cmd = "";
+	cmd += "ref ";
+	cmd += getObjName();
+	sendCommand(cmd.c_str());
+    return recvRetCommandFinished();
+}
+
+uint32_t NexButton::getPicc2(uint32_t *number)
+{
+	String cmd;
+	cmd += "get ";
+	cmd += getObjName();
+	cmd += ".picc2";
+	sendCommand(cmd.c_str());
+	return recvRetNumber(number);
+}
+
+bool NexButton::setPicc2(uint32_t number)
+{
+	char buf[10] = {0};
+    String cmd;
+    
+    utoa(number, buf, 10);
+    cmd += getObjName();
+    cmd += ".picc2=";
+    cmd += buf;
+    sendCommand(cmd.c_str());
+	
+	cmd = "";
+	cmd += "ref ";
+	cmd += getObjName();
+	sendCommand(cmd.c_str());
+    return recvRetCommandFinished();
+}
+
+uint32_t NexButton::getPic(uint32_t *number)
+{
+	String cmd;
+	cmd += "get ";
+	cmd += getObjName();
+	cmd += ".pic";
+	sendCommand(cmd.c_str());
+	return recvRetNumber(number);
+}
+
+bool NexButton::setPic(uint32_t number)
+{
+	char buf[10] = {0};
+    String cmd;
+    
+    utoa(number, buf, 10);
+    cmd += getObjName();
+    cmd += ".pic=";
+    cmd += buf;
+    sendCommand(cmd.c_str());
+	
+	cmd = "";
+	cmd += "ref ";
+	cmd += getObjName();
+	sendCommand(cmd.c_str());
+    return recvRetCommandFinished();
+}
+
+uint32_t NexButton::getPic2(uint32_t *number)
+{
+	String cmd;
+	cmd += "get ";
+	cmd += getObjName();
+	cmd += ".pic2";
+	sendCommand(cmd.c_str());
+	return recvRetNumber(number);
+}
+
+bool NexButton::setPic2(uint32_t number)
+{
+	char buf[10] = {0};
+    String cmd;
+    
+    utoa(number, buf, 10);
+    cmd += getObjName();
+    cmd += ".pic2=";
+    cmd += buf;
+    sendCommand(cmd.c_str());
+	
+	cmd = "";
+	cmd += "ref ";
+	cmd += getObjName();
+	sendCommand(cmd.c_str());
+    return recvRetCommandFinished();
+}
