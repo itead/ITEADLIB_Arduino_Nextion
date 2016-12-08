bf370dbc511f8cb39e352f92fe0268f71efb6f7c
huangxiaoming <xiaoming.huang@itead.cc>
2016-12-08 11:11:43 +0800

    add gpio and rtc example

---
+++ examples/CompRtc/CompRtc.ino
@@ -0,0 +1,19 @@
+#include "Nextion.h"
+
+NexText t0 = NexText(0,1,"t0");
+NexRtc  rtc;
+uint32_t  time[7] = {2016,11,25,12,34,50};
+uint8_t time_buf[30] = {0};
+
+void setup() 
+{
+    nexSerial.begin(115200);
+    rtc.write_rtc_time(time);
+}
+
+void loop() 
+{
+    rtc.read_rtc_time(time_buf,30);
+    t0.setText(time_buf);
+    delay(1000);
+}
